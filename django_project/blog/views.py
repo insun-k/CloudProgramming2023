bf9658b251aa2153c387a1657cebabff08b29902
insun <kis8294@gmail.com>
2023-04-04 15:30:11 +0900

    작성자 추가

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,29 +3,10 @@ from django.views.generic import ListView, DetailView
 
 from .models import Post
 
-
+# Create your views here.
 class PostList(ListView):
     model = Post
     ordering = '-pk'
-
-# Create your views here.
-def index(request):
-    #사용자의 요청을 받아서 이런일 저런일..
-
-    posts = Post.objects.all().order_by('-pk')
-
-    return render(request, 'blog/post_list.html', {
-        'posts' : posts,
-    })
-
-def single_post_page(request, post_num):
-    post = Post.objects.get(pk=post_num)
-
-    return render(
-        request, 'blog/post_detail.html', {
-            'post' : post,
-        }
-    )
 
 
 class PostDetail(DetailView):
